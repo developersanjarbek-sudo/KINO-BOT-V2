687ff00c288782cf7b20366215e1edf556fffe3e
Sanjarbek <sanjarbek@gmail.com>
2026-03-20 10:50:27 +0500

    database error

--- src/config/db.js
+++ src/config/db.js
@@ -1,6 +1,11 @@
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import logger from '../utils/logger.js';
+import dns from 'dns';
+
+// Bazi mahalliy internet provayderlar (masalan Uztelecom) MongoDB domenlarini to'sishi oqibatida 
+// kelib chiquvchi DNS (ENOTFOUND) xatosini mutlaqo aylanib o'tish uchun Google DNS dan foydalanamiz
+dns.setServers(['8.8.8.8', '8.8.4.4', '1.1.1.1']);
 
 dotenv.config();
 
